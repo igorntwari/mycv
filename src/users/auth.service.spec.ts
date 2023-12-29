60a592e393f4d2448b69a394169e1351b9758bed
igorntwari <igorntwari28@gmail.com>
2023-12-29 08:45:10 +0200

    feat: added new files

---
+++ src/users/auth.service.spec.ts
@@ -0,0 +1,51 @@
+import { Test } from '@nestjs/testing';
+import { AuthService } from './auth.service';
+import { UsersService } from './users.service';
+import { User } from './user.entity';
+
+describe('AuthService', () => {
+  let service: AuthService;
+  let fakeUsersService: Partial<UsersService>;
+
+  beforeEach(async () => {
+    // Create a fake copy of the users service
+    const users: User[] = [];
+    fakeUsersService = {
+      find: (email: string) => {
+        const filteredUsers = users.filter((user) => user.email === email);
+        return Promise.resolve(filteredUsers);
+      },
+      create: (email: string, password: string) => {
+        const user = {
+          id: Math.floor(Math.random() * 999999),
+          email,
+          password,
+        } as User;
+        users.push(user);
+        return Promise.resolve(user);
+      },
+    };
+
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        },
+      ],
+    }).compile();
+
+    service = module.get(AuthService);
+  });
+
+  it('can create an instance of auth service', async () => {
+    expect(service).toBeDefined();
+  });
+  it('it creates the user with salted password and hashed password', async () => {
+    const user = await service.signup('igor@i23gmai.com', 'agfag');
+    const [salt, hash] = user.password.split('.');
+    expect(hash).toBeDefined();
+    expect(salt).toBeDefined();
+  });
+});
